545f1a8384d4862d0c32077ad970efe4622c739d
Anastasia <asya_apto@mail.ru>
2021-11-19 22:50:21 +0300

    Change variables case, delete comments

--- main.py
+++ main.py
@@ -12,21 +12,20 @@ from fetch_spacex import fetch_spacex_launch
 if __name__ == "__main__":
     load_dotenv()
 
-    NASA_API_KEY = os.environ['NASA_API_KEY']
-    CHAT_ID = os.environ['CHAT_ID']
+    nasa_api_key = os.environ['NASA_API_KEY']
+    chat_id = os.environ['CHAT_ID']
 
-    # Create /images directory
     pathlib.Path('images/').mkdir(exist_ok=True)
 
     bot = telegram.Bot(token=os.environ["TG_BOT_TOKEN"])
 
     while True:
         fetch_spacex_launch()
-        fetch_nasa_apod(NASA_API_KEY)
-        fetch_epic_img(NASA_API_KEY)
+        fetch_nasa_apod(nasa_api_key)
+        fetch_epic_img(nasa_api_key)
         imgs_to_send = os.listdir("images")
 
         for img in imgs_to_send:
-            bot.send_photo(chat_id=CHAT_ID, photo=open(f"images/{img}", "rb"))
+            bot.send_photo(chat_id=chat_id, photo=open(f"images/{img}", "rb"))
             sleep(int(os.getenv("DELAY", default=86400)))
 
