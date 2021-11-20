1676e777620d9b9574ee9663aa0344509678b80a
Anastasia <asya_apto@mail.ru>
2021-11-20 12:56:12 +0300

    Change var name: json_record to record

--- fetch_nasa.py
+++ fetch_nasa.py
@@ -14,8 +14,8 @@ def fetch_nasa_apod(api_key):
     response = requests.get(nasa_apod_endpoint, params=apod_params)
     response.raise_for_status()
 
-    for number, json_record in enumerate(response.json()):
-        img_url = json_record["url"]
+    for number, record in enumerate(response.json()):
+        img_url = record["url"]
         img_extension = get_extension(img_url)
         download_img(img_url, f"nasa_apod{number}{img_extension}")
 
@@ -24,17 +24,19 @@ def fetch_epic_img(api_key):
     epic_params = {
         "api_key": api_key
     }
-    epic_endpoint = f"https://epic.gsfc.nasa.gov/api/natural"
+    epic_endpoint = f"https://epic.gsfc.nasa" \
+                    f".gov/api/natural"
+
     img_url_template = "https://epic.gsfc.nasa.gov/archive/natural/{year}/{month}/{day}/png/{img_name}.png"
     response = requests.get(epic_endpoint, params=epic_params)
     response.raise_for_status()
 
-    for number, json_record in enumerate(response.json()):
-        img_date = datetime.strptime(json_record["date"], "%Y-%m-%d %H:%M:%S")
+    for number, record in enumerate(response.json()):
+        img_date = datetime.strptime(record["date"], "%Y-%m-%d %H:%M:%S")
         epic_img_url = img_url_template.format(
             year=img_date.year,
             month=img_date.month,
             day=img_date.day,
-            img_name=json_record["image"]
+            img_name=record["image"]
         )
         download_img(epic_img_url, f"epic{number}.png")
